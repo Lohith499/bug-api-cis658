dbe3f2f6737b6a77e1fb24cfbfc8b0fa4a1c0494
Lohith499 <lohith.gre@gmail.com>
2018-04-05 14:31:43 -0400

    pushing validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
-  has_many :bugs  
+  validates :lname, :fname, :email, presence: true
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :email, length: {maximum: 105},
+      uniqueness: { case_sensative: false },
+      format: { with: VALID_EMAIL_REGEX }
+validates :thumbnail, allow_blank: true, format: {with:
+      %r{\.(gif|jpg|png)\Z}i,
+      message: 'must be a URL for GIF, JPG or PNG image.'}
+
+has_many :bugs
 end
--- db/migrate/20180329225337_create_bugs.rb
+++ db/migrate/20180329225337_create_bugs.rb
@@ -3,11 +3,10 @@ class CreateBugs < ActiveRecord::Migration[5.1]
     create_table :bugs do |t|
       t.string :title
       t.text :description
-      t.integer :issue_type
-      t.integer :priority
-      t.integer :status
-
-      t.timestamps
+      t.integer :issue_type, default: 2
+      t.integer :priority, default: 1
+      t.integer :status, default: 0
+      t.timestamps null: false
     end
   end
 end
